cd6bb9327b11a41799e588b7b52c502146f2674a
Kunal Tambekar <kunal@Kunals-MacBook-Pro.local>
2017-10-08 17:37:24 -0700

    Initital Commit till login

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Setup modules and dependencies
+var config = require('./config');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var session = require('cookie-session');
+
+var app = express();
+
+// Configure the session and session storage.
+app.use(session({
+  secret: config.secret,
+  signed: true
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// OAuth2
+var oauth2 = require('./lib/oauth2')(config.oauth2);
+app.use(oauth2.router);
+app.use(oauth2.aware);
+app.use(oauth2.template);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure routes (All in one place)
+app.use('/', require('./lib/routes')());
+// OR like this
+// app.use('/', index);
+// app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Start the server
+var server = app.listen(process.env.PORT || 8080, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
+
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var clientId = '489984240855-nesl58uubgvd1m3ck3mm3rj19cbs13nd.apps.googleusercontent.com';
+var clientSecret = '8lhmKh6JFsV8R4rwHJRTKJ8P';
+var redirectUrl = 'https://8080-dot-3134072-dot-devshell.appspot.com/oauth2callback';
+
+var projectId = 'myvisiontestapp';
+var bucketName = 'myvisiontestapp.';
+
+var credentialsApiKey = 'your-credentials-api-key';
+
+module.exports = {
+  port: process.env.PORT || 8080,
+
+  // Secret is used by sessions to encrypt the cookie.
+  secret: process.env.SESSION_SECRET || 'your-secret-here',
+
+  // The client ID and secret can be obtained by generating a new web
+  // application client ID on Google Developers Console.
+  oauth2: {
+    clientId: process.env.OAUTH_CLIENT_ID || clientId,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET || clientSecret,
+    redirectUrl: process.env.OAUTH2_CALLBACK || redirectUrl,
+    scopes: ['email', 'profile']
+  },
+
+  // Google Developers Console Project Id.
+  gcloud: {
+    projectId: process.env.GCLOUD_PROJECT || projectId
+  },
+
+  gcloudStorageBucket: process.env.CLOUD_BUCKET || bucketName,
+  dataBackend: 'datastore',
+
+  gcloudVision: {
+    key: process.env.CLOUD_VISION_KEY || credentialsApiKey
+  }
+};
